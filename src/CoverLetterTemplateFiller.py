f6777ee7ddd48592176e8548d828ffe43dbc91bf
JamsFM <jams_125@outlook.com>
2025-04-15 19:12:37 -0700

    Incorrectly working; Does not retain font details.

--- src/CoverLetterTemplateFiller.py
+++ src/CoverLetterTemplateFiller.py
@@ -1,11 +1,17 @@
 """Script to Fill a CoverLetter Template"""
 from sys import exit
+from os import path, remove
+import subprocess
+from pathlib import PurePosixPath
 from configparser import ConfigParser
 from logging import config, getLogger
 from json import load
 from shutil import copy2
 from docx import Document
 from docx.shared import Pt
+from re import search, sub
+from time import sleep, ctime
+from datetime import datetime
 
 # Read app folder argument
 app_root = "E:/Dev/Repos/CoverLetterTemplateFiller"
@@ -17,6 +23,15 @@ with open(f'{app_root}/config/dev/logging.json', encoding="UTF-8") as file:
 logger = getLogger(__name__)
 
 
+def docPropSetter(core_props):
+    """Set the author & category of the document"""
+    core_props.author = "James F. Mare"
+    logger.info(f'Successfully set Author of File: [{core_props.author}]')
+    core_props.category = "Cover Letter"
+    logger.info(f'Successfully set Category of File: [{core_props.category}]')
+    logger.info(f'Successfully Filled/Transformed Cover Letter Template File')
+
+
 def docAsStringLogger(paragraphs):
     docAsString = ""
     for paragraph in paragraphs:
@@ -24,6 +39,17 @@ def docAsStringLogger(paragraphs):
     logger.info(f"\n{docAsString}")
 
     logger.info(f'End of File')
+
+
+def fileCopier(srcPath, destPath):
+    """Copy the file at the given source path to the given destination path"""
+    try:
+        copy2(srcPath, destPath)
+        logger.info(f'Successfully Copied Cover Letter Template to destination path!\t[{destPath}]')
+    except Exception as ex:
+        logger.error(f'Following File could not be Copied!\t[{srcPath}]')
+        logger.error(f'Exception:\n[{ex}]')
+        raise
 
 
 def validateParagraphSel(upperInitRef, lowerInitRef, upperCurrRef, lowerCurrRef):
@@ -58,9 +84,7 @@ def fetchUserInput():
     healthcareFlag = False
     if healthcareInput.__eq__("y"):
         healthcareFlag = True
-    elif healthcareInput.__eq__("n"):
-        healthcareFlag = False
-    elif healthcareInput.__eq__(""):
+    elif healthcareInput.__eq__("n") or healthcareInput.__eq__(""):
         healthcareFlag = False
     else:
         raise Exception(f'Failed to receive an input matching [y/n]-\tReceived:\"{healthcareInput}\"')
@@ -80,38 +104,51 @@ def validateParagraphFill(upperCurrRef, lowerCurrRef, upperInitRef, lowerInitRef
 
 def styleParagraph(paragraphs, index, docStyle, docFontName, docFontSize):
     """Style a paragraph with a given style, font name, & font size"""
+    preserveTxtFormatting(paragraphs[index], paragraphs[index].text)
     paragraphs[index].style = docStyle
     paragraphs[index].style.font.name = docFontName
+    #logger.info(f'\t~~~~\t~~~~\t~~~~\t~~~~[{paragraphs[index].style.font.name}]')
+
+    #logger.info(f'\t~~~~\t~~~~\t~~~~\t~~~~[{paragraphs[index].style.font.size}]')
     paragraphs[index].style.font.size = docFontSize
+
+
+def preserveTxtFormatting(paragraph, text):
+    """Preserve text formatting for the first run in a given paragraph"""
+    # Replace text of first run paragraph
+    runs = paragraph.runs
+    if not runs:
+        raise Exception(f'Failed to preserve text formatting for the following paragraph.\n\t\"{paragraph.text}\"')
+
+    runs[0].text = text
+
+    # Delete all remaining runs
+    for run in runs[1:]:
+        r = run._element
+        r.getparent().remove(r)
 
 
 def templateFiller(srcPath, destPath):
     """Fill/Transform the data fields in the Cover Letter Template given Input-Params"""
     doc = Document(srcPath)
-    # Set Author & Category of Doc  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    core_props = doc.core_properties
-    core_props.author = "James F. Mare"
-    logger.info(f'Successfully set Author of File: [{core_props.author}]')
-    core_props.category = "Cover Letter"
-    logger.info(f'Successfully set Category of File: [{core_props.category}]')
-    logger.info(f'Successfully Filled/Transformed Cover Letter Template File')
-
-    #docAsStringLogger(doc.paragraphs)  #  For Debugging  ##############################################################
+    # Set Author & Category of Doc  <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    docPropSetter(doc.core_properties)
 
     # Preparing to transform data in two paragraphs  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    upperIdx, lowerIdx = 4, 8
-    paragraphs = doc.paragraphs
+    paragraphs, upperIdx, lowerIdx = doc.paragraphs, 4, 8
     upperInitRef, lowerInitRef = paragraphs[upperIdx].text, paragraphs[lowerIdx].text
     docStyle, docFontName, docFontSize = paragraphs[upperIdx].style, 'Calibri', Pt(12)
     upperSnippetRef = "I am excited to apply for the [Role] position at [Company]. With a strong foundation in software"
     lowerSnippetRef = "I am eager for the opportunity to bring my problem-solving abilities and technical expertise to"
-    # Run the Validator for Paragraph Selection  <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    # Run the Validator for Paragraph Selection   <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
     validateParagraphSel(upperInitRef, lowerInitRef, upperSnippetRef, lowerSnippetRef)
-    # Health Care Snippet for lower paragraph
-    healthcareSnippet = "am sure my experience coming from the healthcare world could be of some use, and ease my potential transition. I, not only look forward to continuing to make a difference in this world of uncertainty that we find ourselves living in, but I also "
 
-    # Fetch Data from User Input for the Template  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Fetch Data from User Input for the Template   <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
     inputRole, inputCompany, healthcareFlag = fetchUserInput()
+    # Health Care Snippet for lower paragraph  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    healthcareSnippet = ("am sure my experience coming from the healthcare world could be of some use, and ease my "
+                         "potential transition. I, not only look forward to continuing to make a difference in this "
+                         "world of uncertainty that we find ourselves living in, but I also ")
 
     # Fill/Transform the data in the upper paragraph  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     logger.info(f'\nFilling the following paragraph:\n[{paragraphs[upperIdx].text}]')
@@ -122,40 +159,129 @@ def templateFiller(srcPath, destPath):
     paragraphs[upperIdx].text = paragraphs[upperIdx].text.replace("[Company]", inputCompany)
     paragraphs[lowerIdx].text = paragraphs[lowerIdx].text.replace("[Company]", inputCompany)
 
-    if healthcareFlag:
+    if not healthcareFlag:
         paragraphs[lowerIdx].text = paragraphs[lowerIdx].text.replace(healthcareSnippet, "")
 
-    # Validate paragraphs  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Validate paragraphs   <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
     validateParagraphFill(paragraphs[upperIdx].text, paragraphs[lowerIdx].text, upperInitRef, lowerInitRef)
 
-    # Stylize the upper & lower paragraphs  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Stylize the upper & lower paragraphs  <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
     styleParagraph(paragraphs, upperIdx, docStyle, docFontName, docFontSize)
     styleParagraph(paragraphs, lowerIdx, docStyle, docFontName, docFontSize)
 
-    #doc.save(destPath.replace("CL_Dev", "CL_Dev_Test"))  #  For Debugging  ############################################
     doc.save(destPath)
     logger.info(f'Successfully Filled Cover Letter Template File')
     return 0
 
 
+def wslMntr(winPath):
+    """Takes a Windows Path and converts it to a WSL Mounted Path
+        "{driveLetter}:/" -> /mnt/{driveLetter}/
+        cp /mnt/c/Windows/Fonts/calibri.ttf home/jamesfm/.fonts/
+    """
+    winDrive = search("[A-Z]:/", winPath)[0]
+    if winDrive:
+        driveLetter = winDrive[0].lower()
+        mountedDrive = f'/mnt/{driveLetter}/'
+        return PurePosixPath(sub(winDrive, mountedDrive, winPath, 1))
+    else:
+        raise FileNotFoundError(f'Invalid Windows Path: [{winPath}]; Expected Format: [<DriveLetter>:/..]')
+
+
+def docxToPDF(filePath, pdfPath):
+    """Save the docx file as a pdf file"""
+    """
+        /usr/lib/libreoffice/program/soffice.bin --headless \
+            "-env:UserInstallation=file:///tmp/LibreOffice_Conversion_jamesfm" \
+            --convert-to pdf:writer_pdf_Export /mnt/e/Dev/Repos/CoverLetterTemplateFiller/Out/Mare_James_CL_Dev.docx \
+            --outdir /mnt/e/Dev/Repos/CoverLetterTemplateFiller/Out
+        convert /mnt/e/Dev/Repos/CoverLetterTemplateFiller/Out/Mare_James_CL_Dev.docx
+            as a Writer document -> /mnt/e/Dev/Repos/CoverLetterTemplateFiller/Out/Mare_James_CL_Dev.pdf
+            using filter : writer_pdf_Export
+    """
+    try:
+        filePath = wslMntr(str(filePath))
+        destDir = filePath.parent
+        logger.info(f'Attempting to Convert Docx File @[{pdfPath}]\nto PDF using [{filePath}]')
+        bash = ('wsl \
+                /usr/lib/libreoffice/program/soffice.bin \
+                --headless \
+                "-env:UserInstallation=file:///tmp/LibreOffice_Conversion_${USER}" '
+                f'--convert-to pdf:writer_pdf_Export {filePath} \
+                --outdir {destDir}')
+        results = subprocess.Popen(bash, shell=True, text=True)
+        '''bash = ["wsl",
+                "/usr/lib/libreoffice/program/soffice.bin",
+                "--headless",
+                "-env:UserInstallation=file:///tmp/LibreOffice_Conversion_${USER}",
+                "--convert-to", "pdf:writer_pdf_Export", f"{filePath}",
+                "--outdir", f"{destDir}"]
+        results = subprocess.run(bash, text=True)'''
+        results.wait(100.0)
+        #results.wait()
+        #sleep(5)
+        results, error = results.communicate()
+        logger.info(f'SubProcess Results:\n[{results}; {error}]')
+
+        return pdfPath
+
+    except Exception as ex:
+        logger.error(f'Following File could not be Converted to PDF!\t[{filePath}]')
+        raise ex
+
+
+def reattemptDocxToPDF(docPath):
+    """Reattempt to save the docx file as a pdf file for a maximum number of times"""
+    try:
+        pdfPlannedPath = str(docPath).replace(".docx", ".pdf")
+        if path.exists(pdfPlannedPath):
+            remove(pdfPlannedPath)
+        pdfPath = docxToPDF(docPath, pdfPlannedPath)
+
+        maxAttempts = 7
+        for count in range(maxAttempts):
+            if path.exists(pdfPath):
+                currentTime, mTimeStamp = datetime.now().replace(microsecond=0), path.getmtime(pdfPath)
+                mDateTime = datetime.fromtimestamp(mTimeStamp).replace(microsecond=0)
+                recentlyModifiedFlag = currentTime == mDateTime
+                if recentlyModifiedFlag:
+                    logger.info(f'Successfully Converted Docx  File to PDF @ [{pdfPath}]')
+                    return pdfPath
+            else:
+                # Run the docx To PDF Method  <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+                pdfPath = docxToPDF(docPath, pdfPlannedPath)
+                currentTime, mTimeStamp = datetime.now(), path.getmtime(pdfPath)
+
+                if count == maxAttempts:
+                    legibleTime = ctime(mTimeStamp)
+                    raise Exception(f'Newly Generated PDF could not be found! [{pdfPath}]; Or is not new,\n'
+                                    f'given its mTimeStamp: [{legibleTime}] and currentTime: [{currentTime}]')
+    except Exception as ex:
+        logger.error(f'Following File could not be Converted to PDF!\t[{docPath}]')
+        raise ex
+
+
 def templator(config):
     """Create fresh copy of Cover Letter Template in /Out Dir for use in the templateFiller"""
     # Initialize Names for use in the copy method  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    srcFileName = config.get("templator.properties", "SRC_FILE_NAME")
-    destFileName = config.get("templator.properties", "DEST_FILE_NAME")
+    docSrcFileName = config.get("templator.properties", "DOC_SRC_FILE_NAME")
+    docDestFileName = config.get("templator.properties", "DOC_DEST_FILE_NAME")
     # Initialize Paths for use in the copy method  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    srcPath = f"{app_root}/In/{srcFileName}"
-    destPath = f"{app_root}/Out/{destFileName}"
-    try:
-        copy2(srcPath, destPath)  # Commented out for testing purposes  <><><><><><><><><><><><><><><><><><><><><><><><>
-        logger.info(f'Successfully Copied Cover Letter Template to destination path!\t[{destPath}]')
-    except Exception as ex:
-        logger.error(f'Following File could not be Copied!\t[{srcPath}]')
-        logger.error(f'Exception:\n[{ex}]')
-        raise
+    docSrcPath = PurePosixPath(f"{app_root}/In/{docSrcFileName}")
+    docDestPath = PurePosixPath(f"{app_root}/Out/{docDestFileName}")
 
-    # Run the Template Filler Next  <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
-    templateFiller(srcPath, destPath)
+    # Run the File Copier Method  <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    fileCopier(docSrcPath, docDestPath)
+
+    # Run the Template Filler Method  <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    templateFiller(docSrcPath, docDestPath)
+    # Run the docx To PDF Method  <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    pdfFile = reattemptDocxToPDF(docDestPath)
+
+    # Run the File Copier Method  <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+    pdfDestDir = config.get("templator.properties", "PDF_FINAL_DEST_DIR")
+    pdfDestPath = f'{pdfDestDir}{docDestFileName.replace(".docx", ".pdf")}'
+    fileCopier(pdfFile, pdfDestPath)
 
     logger.info(f'Successfully Filled Template')
     return 0
